c8a1886b1bd3d8a10d6359c31939eba3394dd540
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-01-08 23:57:14 +0200

    usage modules in Vuex store

--- src/store/store.js
+++ src/store/store.js
@@ -1,27 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import products from './modules/products'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-	state: {
-		products: {}
-	},
-	mutations: {
-		'SET_STORE'(state, products) {
-			state.products = products
-		}
-	},
-	actions: {
-		initStore: ({commit}) => {
-			axios.get('static/products.json')
-				.then(response => {
-					commit('SET_STORE', response.data.products)
-				})
-				.catch(error => console.log(error + " Check path to json"))
-		}
-	},
-	getters: {
-		products: state => state.products
+	namespaced: true,
+	modules: {
+		products
 	}
 });
